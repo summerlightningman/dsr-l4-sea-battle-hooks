537fb51768fbd55675d07c4b59b24c1c730c9d54
summerlightningman <div8@bk.ru>
2022-02-18 21:12:23 +0300

    added component for confirmation screen

---
+++ src/components/confirmation-screen.tsx
@@ -0,0 +1,14 @@
+import {Component} from "react";
+import {ConfirmationScreenProps, ConfirmationScreenState} from "../types/confirmation-screen";
+
+import '../styles/confirmation-screen.css';
+
+class ConfirmationScreen extends Component<ConfirmationScreenProps, ConfirmationScreenState> {
+    render() {
+        return <div className="confirmation-screen">
+
+        </div>
+    }
+}
+
+export default ConfirmationScreen
---
+++ src/types/confirmation-screen.ts
@@ -0,0 +1,7 @@
+export interface ConfirmationScreenProps {
+
+}
+
+export interface ConfirmationScreenState {
+
+}
